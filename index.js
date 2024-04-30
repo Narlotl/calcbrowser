1bd8c1855564b9dd5d3d7ede9ca7abb468f71b3f
Narlotl <fretwellcalifornia@gmail.com>
2024-04-30 06:47:44 -0700

    Move unknown bytes to start of data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { writeFileSync } = require('fs');
+const { writeFileSync, readFileSync } = require('fs');
 const { decode } = require('png-js');
 const puppeteer = require('puppeteer');
 
@@ -6,7 +6,8 @@ const puppeteer = require('puppeteer');
     // Initialize browser
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 133 * 10, height: 83 * 10 });
+    const SCALE = 7;
+    await page.setViewport({ width: 133 * SCALE, height: 83 * SCALE });
 
     await page.goto('https://example.com');
 
@@ -14,18 +15,19 @@ const puppeteer = require('puppeteer');
 
     // Read pixels
     decode('page.png', async (pixels) => {
-        let data = [];
+        // These bytes always begin the image data section and must be counted into the data length
+        let data = [0xE5, 0x56, 0x81];
         // Compress image by 10 times to fit calculator screen
-        for (let y = 820; y >= 0; y -= 10) {
-            for (let x = 0; x < 1330; x += 10) {
+        for (let y = 82 * SCALE; y >= 0; y -= SCALE) {
+            for (let x = 0; x < 133 * SCALE; x += SCALE) {
                 let count = r = g = b = 0;
-                for (let i = 0; i < 10; i++)
-                    for (let j = 0; j < 10; j++) {
-                        const i = (y * 1330 + x) * 4;
+                for (let i = 0; i < SCALE; i++)
+                    for (let j = 0; j < SCALE && x + j < 133 * SCALE; j++) {
+                        const k = ((y + i) * (133 * SCALE) + (x + j)) * 4;
                         // Convert to 16-bit color
-                        r += Math.round(pixels[i] * 31 / 255);
-                        g += Math.round(pixels[i + 1] * 63 / 255);
-                        b += Math.round(pixels[i + 2] * 31 / 255);
+                        r += Math.round(pixels[k] * 31 / 255);
+                        g += Math.round(pixels[k + 1] * 63 / 255);
+                        b += Math.round(pixels[k + 2] * 31 / 255);
                         count++;
                     }
 
@@ -35,8 +37,9 @@ const puppeteer = require('puppeteer');
             // Row terminator
             data.push(0xFF, 0xFF);
         }
+
         // Data header
-        data = [
+        const header = [
             // Constant
             0x0D,
             0x00,
@@ -61,11 +64,8 @@ const puppeteer = require('puppeteer');
             // Repeat of length
             data.length & 0xFF,
             data.length >> 8,
-            // Unknown bytes
-            0xE5,
-            0x56,
-            0x81,
-        ].concat(data)
+        ];
+        data = header.concat(data);
 
         const metadata = [
             // Signature
@@ -137,6 +137,7 @@ const puppeteer = require('puppeteer');
         data.push(checksum & 255, checksum >> 8 & 255);
 
         writeFileSync('Image6.8ca', Buffer.from(data));
+        writeFileSync('Image6.txt', data.map(x => x.toString(16).padStart(2, '0')).join('\n'));
 
         await browser.close();
     })
